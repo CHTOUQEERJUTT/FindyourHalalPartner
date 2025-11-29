5b896e5633a5019d201f4c71c9ea4795d8934d5c
Mohammad Touqeer <149080891+CHTOUQEERJUTT@users.noreply.github.com.>
2025-11-29 21:28:28 +0500

    next config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  // Remove output: "standalone" and outputFileTracing
   images: {
     remotePatterns: [
       {
